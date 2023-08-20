da2c1f1a158430b1de4d1bcac1f749dcdc51edd8
paolo-petralia <ptrpla03a05c351d@studium.unict.it>
2023-08-21 00:58:48 +0200

    Added lazygit in toggleterm.lua

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -3,3 +3,12 @@ require("toggleterm").setup {
     direction = 'float',
     size = 50,
 }
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.keymap.set('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
